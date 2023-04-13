e07bedc0eff9689fb80f7a12e3b659bcf68a341f
Coding_Dynasty <eljerrariali8@gmail.com>
2023-04-13 23:58:49 +0000

    added snake and food

--- Snake.js
+++ Snake.js
@@ -5,4 +5,30 @@ class Snake{
    this.xSpeed = 1
    this.ySpeed = 0
  }
+ 
+ update(){
+    this.x += this.xSpeed * scale
+    this.y += this.ySpeed * scale
+    this.x = constrain(this.x, 0, width-scale)
+    this.y = constrain(this.y, 0, height-scale)
+ }
+ 
+ show(){
+   fill(255)
+   rect(this.x, this.y, scale, scale) 
+ }
+ 
+ dir(x, y){
+   this.xSpeed = x
+   this.ySpeed = y
+ }
+ 
+ eat(pos){
+  const d = dist(this.x, this.y, pos.x, pos.y)
+  if(d < 1){
+    return true
+  }else{
+   return false 
+  }
+ }
 }
--- snake_game.js
+++ snake_game.js
@@ -1,9 +1,45 @@
+let snake;
+const scale = 20;
+
+let food;
+
 function setup() {
-  createCanvas(400,400)
+  createCanvas(600,600)
+  frameRate(10)
+  snake = new Snake(200,200)
+   pickLocation()
 }
 
+function pickLocation(){
+   const cols = floor(width/scale)
+   const rows = floor(height/scale)
+   food = createVector(floor(random(cols)), floor(random(rows)))
+   food.mult(scale)
+
+
+}
 
 function draw() {
   background(0)
-  ellipse(200,200,20)
+  snake.update()
+  snake.show()
+  if(snake.eat(food)){
+    pickLocation()
+  }
+  ellipse(food.x, food.y, scale, scale)
+}
+
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    snake.dir(0, -1)
+  }else if(keyCode === DOWN_ARROW){
+    snake.dir(0,1)
+  }
+  else if(keyCode === LEFT_ARROW){
+    snake.dir(-1,0)
+  }
+  else if(keyCode === RIGHT_ARROW){
+    snake.dir(1,0)
+  }
 }
