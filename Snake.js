5004f9b0ee2bc4e8ed92d0e4de8890db9e794448
Coding_Dynasty <eljerrariali8@gmail.com>
2023-04-14 00:34:15 +0000

    some formatting

--- Snake.js
+++ Snake.js
@@ -1,60 +1,59 @@
-class Snake{
- constructor(x,y){
-   this.x = x
-   this.y = y
-   this.xSpeed = 1
-   this.ySpeed = 0
-   this.total = 0
-   this.tail = []
- }
- 
- update(){
-   if(this.total === this.tail.length){
-     for(let i =0; i < this.tail.length-1; i++){
-       this.tail[i]=this.tail[i+1]
-     }
-   }
-     this.tail[this.total-1] = createVector(this.x, this.y)
-   
-    this.x += this.xSpeed * scale
-    this.y += this.ySpeed * scale
-    
-    this.x = constrain(this.x, 0, width-scale)
-    this.y = constrain(this.y, 0, height-scale)
- }
- 
- show(){
-    fill(255)
-   for(let i =0; i < this.tail.length; i++){
-     rect(this.tail[i].x, this.tail[i].y, scale, scale) 
+class Snake {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.xSpeed = 1;
+    this.ySpeed = 0;
+    this.total = 0;
+    this.tail = [];
+  }
 
-   }
-   rect(this.x, this.y, scale, scale) 
- }
- 
- dir(x, y){
-   this.xSpeed = x
-   this.ySpeed = y
- }
- 
- eat(pos){
-  const d = dist(this.x, this.y, pos.x, pos.y)
-  if(d < 1){
-    this.total++
-    return true
-  }else{
-   return false 
+  update() {
+    if (this.total === this.tail.length) {
+      for (let i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+    }
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
+    this.x += this.xSpeed * scale;
+    this.y += this.ySpeed * scale;
+
+    this.x = constrain(this.x, 0, width - scale);
+    this.y = constrain(this.y, 0, height - scale);
   }
- }
- 
- death(){
-  for( let i =0; i< this.tail.length; i++){
-     const pos = this.tail[i]
-     const d = dist(this.x, this.y, pos.x, pos.y)
-     if(d < 1){
-       this.total = 0
-       this.tail=[]
-   }     
+
+  show() {
+    fill(255);
+    for (let i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, scale, scale);
+    }
+    rect(this.x, this.y, scale, scale);
   }
- }
+
+  dir(x, y) {
+    this.xSpeed = x;
+    this.ySpeed = y;
+  }
+
+  eat(pos) {
+    const d = dist(this.x, this.y, pos.x, pos.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  death() {
+    for (let i = 0; i < this.tail.length; i++) {
+      const pos = this.tail[i];
+      const d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1) {
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
 }
--- snake_game.js
+++ snake_game.js
@@ -4,50 +4,45 @@ const scale = 20;
 let food;
 
 function setup() {
-  createCanvas(600,600)
-  frameRate(10)
-  snake = new Snake(200,200)
-   pickLocation()
+  createCanvas(600, 600);
+  frameRate(10);
+  snake = new Snake(200, 200);
+  pickLocation();
 }
 
-function pickLocation(){
-   const cols = floor(width/scale)
-   const rows = floor(height/scale)
-   food = createVector(floor(random(cols)), floor(random(rows)))
-   food.mult(scale)
-
-
+function pickLocation() {
+  const cols = floor(width / scale);
+  const rows = floor(height / scale);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scale);
 }
 
 function draw() {
-  background(0)
-  
-    if(snake.eat(food)){
-    pickLocation()
-  }
-  
-  snake.death()
-  snake.update()
-  snake.show()
+  background(0);
 
-  rect(food.x, food.y, scale, scale)
+  if (snake.eat(food)) {
+    pickLocation();
+  }
+
+  snake.death();
+  snake.update();
+  snake.show();
+
+  rect(food.x, food.y, scale, scale);
 }
 
-function mousePressed(){
-    snake.total++
+function mousePressed() {
+  snake.total++;
 }
 
-
-function keyPressed(){
-  if(keyCode === UP_ARROW){
-    snake.dir(0, -1)
-  }else if(keyCode === DOWN_ARROW){
-    snake.dir(0,1)
-  }
-  else if(keyCode === LEFT_ARROW){
-    snake.dir(-1,0)
-  }
-  else if(keyCode === RIGHT_ARROW){
-    snake.dir(1,0)
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.dir(0, 1);
+  } else if (keyCode === LEFT_ARROW) {
+    snake.dir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+    snake.dir(1, 0);
   }
 }
